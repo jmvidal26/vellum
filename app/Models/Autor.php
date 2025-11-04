821f441a0a4ccaad0b5e9aea0ea4dd64991c6b6d
isahlopess <isadorafl05@gmail.com>
2025-11-04 08:05:37 -0400

    feat: ajuste formatação do nome do autor (nome e sobrenome)

--- app/Models/Autor.php
+++ app/Models/Autor.php
@@ -6,15 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Autor extends Model
 {
-   protected $table = 'autores';
+    protected $table = 'autores';
 
     protected $fillable = [
         'nome',
         'dt_nascimento',
         'dt_falecimento'
     ];
-    
+
     public function livros(){
         return $this->belongsToMany(Livro::class);
     }
+
+    public function getNomeAttribute($value)
+    {
+        $parts = explode(', ', $value);
+        if (count($parts) == 2) {
+            return $parts[1] . ' ' . $parts[0];
+        }
+        return $value;
+    }
 }
+
